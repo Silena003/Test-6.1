e0159fc2d343a57e219c5a3511745bd517e90044
Elena <silena003@gmail.com>
2021-11-10 11:20:02 +0300

    Initial commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
-    @org.junit.jupiter.api.Test
-    void calculatregistered() {
+    @Test
+    void shouldCalculateWhenRegisteredAndBonusUnderLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
         long amount = 1000_60;
@@ -16,16 +18,30 @@ class BonusServiceTest {
 
 
     }
-    @org.junit.jupiter.api.Test
-    void calculatnotregistered() {
+    @Test
+    void shouldСalculateWhenRegisteredAndBonusMoreThanLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
-        long amount = 1060;
+        long amount = 1_000_000_60;
         boolean registered = true;
-        long expected = 30;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+
+
+    }
+    @Test
+    void calculateNotRegistered() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean notregistered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, notregistered);
         assertEquals(expected, actual);
 
 
